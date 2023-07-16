6ea5951f7bfcff739c7b46f4a8a3806948b6a7d4
martinaamilad <martina.m.mikhaeel@gmail.com>
2023-07-16 14:42:41 -0700

    Try

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c)
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
