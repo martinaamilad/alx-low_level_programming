9bc04525938026b8486a59abf832d24f4e77557e
martinaamilad <martina.m.mikhaeel@gmail.com>
2023-07-16 14:46:19 -0700

    Try

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,14 +9,14 @@ int main(void)
 {
 	char c;
 
-	for (c = '0'; c <= '9'; c++);
+	for (c = '0'; c <= '9'; c++)
 	{
 		putchar(c);
 	}
 
-	for (c = 'a'; c <= 'z'; c++);
+	for (c = 'a'; c <= 'z'; c++)
 	{
-		putchar(c)
+		putchar(c);
 	}
 	
 	putchar('\n');
