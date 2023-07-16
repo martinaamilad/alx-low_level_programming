a5db06ceaf750bab15882990910c80e612271e0e
martinaamilad <martina.m.mikhaeel@gmail.com>
2023-07-16 13:17:19 -0700

    Try

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.c>
+/**
+ * main - print number base 10
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char n,
+
+	     for (n = '0'; n <= '9'; n++)
+	     {
+		     putchar(n);
+	     }
+
+	putchar('\n');
+
+	return (0);
+}
