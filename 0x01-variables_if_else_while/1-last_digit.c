4203f701a5e8fac1f399f99d10d43da2fd5320dc
martinaamilad <martina.m.mikhaeel@gmail.com>
2023-07-16 08:58:23 -0700

    Try

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,7 +6,6 @@
 /**
  * main - Prints a text according number
  *
- * Disription: generate random numbers
  * Reurn: Always 0 (Success)
  */
 int main(void)
@@ -25,7 +24,7 @@ int main(void)
 	{
 		printf("last digit of %d is %d and is 0\n", n);
 	}
-	else (lastd > 6 && lastd != 0)
+	else if (lastd > 6 && lastd != 0)
 	{
 		printf("last digit of %d is %d and is less than 6 and not 0\n", n);
 	}
