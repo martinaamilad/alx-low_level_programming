e55b4ec3fcc1a5a058a32ac70c9a3bad184ebf94
martinaamilad <martina.m.mikhaeel@gmail.com>
2023-07-17 08:53:16 -0700

    Try

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,13 +9,13 @@ int main(void)
 {
 	int c, i, e, k;
 	
-	for (c = 48, c <= 57; c++)
+	for (c = 48; c <= 57; c++)
 	{
-		for (i = 48, i <= 57; i++)
+		for (i = 48; i <= 57; i++)
 		{
-			for (e = 48, e <= 57; e++)
+			for (e = 48; e <= 57; e++)
 			{
-				for (k = 48, k <= 57; k++)
+				for (k = 48; k <= 57; k++)
 				{
 					if (((e + k) > (c + i) && e >= c) || c < e)
 					{
