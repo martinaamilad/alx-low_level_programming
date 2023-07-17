f815b6b41ac274dc72f84942bf23c3d91e806b97
martinaamilad <martina.m.mikhaeel@gmail.com>
2023-07-17 05:57:45 -0700

    Try

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,15 +8,15 @@
 int main(void)
 {
 	char c;
-
+	
 	for (c = 'a'; c <= 'z'; c++)
 	{
-		putchar (c);
+		putchar(c);
 	}
 
 	for (c = 'A'; c <= 'Z'; c++)
 	{
-		putchar (c);
+		putchar(c);
 	}
 
 	putchar('\n');
