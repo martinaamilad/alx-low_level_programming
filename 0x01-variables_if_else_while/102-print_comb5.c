75c865573b53b80ab871a60962a3e6f17e228865
martinaamilad <martina.m.mikhaeel@gmail.com>
2023-07-17 08:51:58 -0700

    Try

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+/**
+ * main - print comibnations of number
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int c, i, e, k;
+	
+	for (c = 48, c <= 57; c++)
+	{
+		for (i = 48, i <= 57; i++)
+		{
+			for (e = 48, e <= 57; e++)
+			{
+				for (k = 48, k <= 57; k++)
+				{
+					if (((e + k) > (c + i) && e >= c) || c < e)
+					{
+						putchar(c);
+						putchar(i);
+						putchar(' ');
+						putchar(e);
+						putchar(k);
+
+						if (c + i + e + k == 277 && c == 57)
+						{
+							break;
+						}
+						else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+
+
