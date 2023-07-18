7f67c831b5d69bce8f77309deee9be01f665d3b8
martinaamilad <martina.m.mikhaeel@gmail.com>
2023-07-18 08:18:02 -0700

    Try

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - make alphabet
+ *
+ * Return: void
+ */
+void print_alphabet(void);
+{
+	char i;
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			putchar(i);
+		}
+		putchar('\n');
+	}
