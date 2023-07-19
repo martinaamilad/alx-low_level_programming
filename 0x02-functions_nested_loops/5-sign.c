46816555aee1bd8dff7b77289b2b27214c97a749
martinaamilad <martina.m.mikhaeel@gmail.com>
2023-07-19 05:33:31 -0700

    Try

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
+ * print_sign - print the signs
+ * @c: The number to be checked
  *
- *
- *
+ * Return: 1 for pesitive num, -1 for negative num or 0 for anything else
  */
 int print_sign(int n)
 {
-	if (c > 0)
+	if (n > 0)
 	{
 		_putchar(43);
 		return (1);
 	}
-	else if (c > 0)
+	else if (n > 0)
 	{
 		_putchar(45);
 		return (-1);
