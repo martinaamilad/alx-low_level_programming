25537df19104edc0b9910c4a49bbe4ce4b11b4b9
martinaamilad <martina.m.mikhaeel@gmail.com>
2023-07-19 05:29:18 -0700

    Try

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+int print_sign(int n)
+{
+	if (c > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (c > 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
