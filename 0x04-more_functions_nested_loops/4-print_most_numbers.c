06248b773fced9b3e5fa16f420ea45a5167e1d37
martinaamilad <martina.m.mikhaeel@gmail.com>
2023-07-20 12:33:21 -0700

    Try

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,19 +9,19 @@
  */ 
 void print_most_numbers(void)
 {
-	int n;
+	int n = 0;
 
-	for (n = 0; n <= 9; n++)
+	for (; n <= 9; n++)
 	{
-		if (n == 2 || n ==4)
+		if (n == 2 || n == 4)
 		{
 			continue;
 		}
 		else
 		{
-			putchar(n + '0');
+			_putchar(n + '0');
 		}
 	}
 
-	putchar('\n');
+	_putchar('\n');
 }
