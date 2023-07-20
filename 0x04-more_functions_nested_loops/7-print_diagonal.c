5e97fb38bae03ab3e8c9779bccb1299eaf7579f0
martinaamilad <martina.m.mikhaeel@gmail.com>
2023-07-20 15:07:31 -0700

    Try

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,1 +1,30 @@
 #include "main.h"
+
+/**
+ * print_diagonal - Draws a diagonal line
+ *@n: The number times to be print
+ *
+ * Return: empty
+ */
+void print_diagonal(int n)
+{
+	int x, y;
+
+	if (n <- 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			for (y = 0; y <x; y++)
+			{
+				_putchar(32);
+			}
+
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
