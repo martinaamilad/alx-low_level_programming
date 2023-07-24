18f37397a3105891daed4c84d03c119c0fda3d11
martinaamilad <martina.m.mikhaeel@gmail.com>
2023-07-24 16:46:03 -0700

    Try

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - print a string
+ * @str: the string to print
+ *
+ * Return: void
+ */
+void _puts(char *str);
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+
+	_putchar('\n');
+}
