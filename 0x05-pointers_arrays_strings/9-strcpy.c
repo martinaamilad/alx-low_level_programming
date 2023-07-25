ff733bb72a858f7e4740e37d0a1601eedfc9cfc7
martinaamilad <martina.m.mikhaeel@gmail.com>
2023-07-25 07:08:15 -0700

    Try

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - is to copy
+ * @src: the variable
+ *
+ * Return: Always success
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i] = '\0';
+
+	return dest;
+}
