bf24bc29be95892819364e1b076f5342aead1f41
martinaamilad <martina.m.mikhaeel@gmail.com>
2023-07-24 23:49:53 -0700

    Try

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
+ * print_rev - to print in reverse
+ * @s: the variable
  *
- *
+ * Return: void
  */
 void print_rev(char *s)
 {
 	int len = strlen(s);
 
-	for (int i = len - 1; i >= 0; i --)
+	for (int i = len - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
