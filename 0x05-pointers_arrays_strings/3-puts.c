162bc615077958ae1538b902acd7b9e5af26e6f3
martinaamilad <martina.m.mikhaeel@gmail.com>
2023-07-24 22:28:00 -0700

    Try

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _puts - print a string
@@ -10,8 +11,8 @@ void _puts(char *str);
 {
 	while (*str != '\0')
 	{
-		_putchar(*str++);
+		putchar(*str++);
 	}
 
-	_putchar('\n');
+	putchar('\n');
 }
