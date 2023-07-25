7592f67f7b1a891d63650ac5453ec46bb05aee75
martinaamilad <martina.m.mikhaeel@gmail.com>
2023-07-25 04:37:30 -0700

    Try

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_array - to print the element inside yhe array
+ * @a: the variable
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+
+	printf("\n");
+}
