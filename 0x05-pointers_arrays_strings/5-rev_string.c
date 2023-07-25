287257d0879a8d6edca9466b4463ea09e89b10a0
martinaamilad <martina.m.mikhaeel@gmail.com>
2023-07-25 14:42:14 -0700

    Try

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,10 +12,12 @@ void rev_string(char *s)
 {
 	int i, j;
 
-	for (i = 0; j = strlen(s) - 1; i < j; i++; j--)
+	for (i = 0, j = strlen(s) - 1; i < j; i++; j--)
 	{
 		char temp = s[i];
 		s[i] = s[j];
 		s[j] = temp;
 	}
+
+	return (void);
 }
