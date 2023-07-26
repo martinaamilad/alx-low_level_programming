82944325202fb7455aa8a4335952517ce4334979
martinaamilad <martina.m.mikhaeel@gmail.com>
2023-07-26 14:42:47 -0700

    TRy

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *
+ *
+ */
+char *_strcat(char *dest, char *src)
+{
+	int dest_len = strlen(dest);
+	int src_len = strlen(src);
+	char *dest_ptr = dest + dest_len;
+
+	int i;
+
+	for (i = 0; i < scr_len; i++)
+	{
+		*dest_ptr++ = src[i];
+	}
+
+	*dest_ptr = '\0';
+
+	return (dest);
+}
