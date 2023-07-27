d445ba99f54b1ed705a83667188fc0b91288fbb1
martinaamilad <martina.m.mikhaeel@gmail.com>
2023-07-27 13:19:02 -0700

    Try

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare two strings
+ * @s1: input value
+ * @s2: input value
+ *
+ * Return: s1[i] - s2[i]
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int k;
+
+	k = 0;
+	while (s1[k] != '\0' && s2[k] != '\0')
+	{
+		if (s1[k] != s2[k])
+		{
+			return (s1[k] - s2[k]);
+		}
+		k++;
+	}
+	return (0);
+}
