48aaabf35743eee414e53b8f41a7e31c0d8cb1f3
martinaamilad <martina.m.mikhaeel@gmail.com>
2023-07-31 06:43:29 -0700

    Try

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,1 +1,20 @@
 #include "main.h"
+
+/**
+ * _memset - to fill the memory with new value
+ * @*s: pointer 
+ * @b: the value
+ *
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned char *p = (unsigned char *)s;
+
+	while (n--)
+	{
+		*p++ = b;
+	}
+
+	return (s);
+}
