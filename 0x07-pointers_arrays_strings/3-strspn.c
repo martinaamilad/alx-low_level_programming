da2b1ad78f3cd1bc6cd81c70152f9bfebcdad2b9
martinaamilad <136886410+martinaamilad@users.noreply.github.com>
2023-07-31 22:58:38 +0300

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -19,12 +19,12 @@ unsigned int _strspn(char *s, char *accept)
     j=0;
     while(*(accept+j)){
       if(*(s+i) == *(accept+j)){
-        break; //Found a match.
+        break;
       }
       j++;
     }
     if(!*(accept+j)){
-      return i; //No match found.
+      return i;
     }
     i++;
   }
