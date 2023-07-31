f5118ab912dcdf1c325ae4461dc0755669c417c3
martinaamilad <136886410+martinaamilad@users.noreply.github.com>
2023-07-31 22:51:38 +0300

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,10 +1,39 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
+ * _strspn - gets the length of a prefix substring
  *
+ * @s: segment to return bytes from
+ * @accept: the bytes to include
  *
- *
+ * Return: the number of bytes in the initial segment of @s which consist only
+ * of bytes from @accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
+    unsigned int i;
+    int found;
+
+    i = 0;
+    while (s[i])
+    {
+        found = 0;
+        for (const char *p = accept; *p; p++)
+        {
+            if (s[i] == *p)
+            {
+                found = 1;
+                break;
+            }
+        }
+        if (!found)
+        {
+            break;
+        }
+        i++;
+    }
+
+    return i;
+}
 	
