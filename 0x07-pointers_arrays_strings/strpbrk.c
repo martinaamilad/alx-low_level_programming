857982fd8f30f4365d5136e396f26fc8f083aa65
martinaamilad <martina.m.mikhaeel@gmail.com>
2023-08-01 09:31:43 -0700

    Try

---
+++ 0x07-pointers_arrays_strings/strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int j;
+
+	if (i[s] = j[accept])
+	{
+		return (*s);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
+
