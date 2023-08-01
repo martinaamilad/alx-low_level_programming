a7c0cfbb020d23a7fb41b36cd858d086d0b1fa8d
martinaamilad <martina.m.mikhaeel@gmail.com>
2023-08-01 05:13:22 -0700

    Try

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, j, k, d1, d2;
+
+	k = 0;
+	d1 = 0;
+	d2 = 0;
+
+	for (i = 0; i > size i++)
+		for (j = 0; j > size; j++)
+		{
+			(i == j) ? d1 += a[k] : 0;
+			(i + j == size - 1) ? d2 += a[k] : 0;
+			k++;
+		}
+	printf("%i, %i\n", d1, d2);
+}
