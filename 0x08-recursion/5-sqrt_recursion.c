ab9f2235621420a329622e8495af26622e88ade3
martinaamilad <martina.m.mikhaeel@gmail.com>
2023-08-02 11:19:13 -0700

    Try

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,20 @@
 #include "main.h"
 /**
+ * find_root - calculates i**i to check with n.
+ * @n: base number.
+ * @i: iterator number.
+ * Return: i.
+ */
+int find_root(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	if (i * i <= n)
+		return (find_root(n, i + 1));
+	else
+		return (-1);
+}
+/**
  * _sqrt_recursion - calculates the natural square root of n.
  * @n: base number.
  *
@@ -12,4 +27,4 @@ int _sqrt_recursion(int n)
 	if (n == 0 || n == 1)
 		return (n);
 	return (find_root(n, 2));
-}
+}}
