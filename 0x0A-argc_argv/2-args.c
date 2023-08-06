0e14f657a716ec480dc4493aaa7835bec06c2390
martinaamilad <martina.m.mikhaeel@gmail.com>
2023-08-06 14:47:39 -0700

    TRY

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print the program name
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	int n;
+	while (argc --)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
