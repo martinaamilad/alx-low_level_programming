c5ad346cd8b43d897d778c2b84dcfba782e0fed5
martinaamilad <martina.m.mikhaeel@gmail.com>
2023-08-06 11:48:29 -0700

    Try

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print theprogram name
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+int main(int argc, char *agrv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
