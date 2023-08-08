022b86f7c06e96f5b7a76c6fd8230e45178ba60f
martinaamilad <martina.m.mikhaeel@gmail.com>
2023-08-08 12:33:10 -0700

    Try

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-ar -rcs liball.a *.c
+
+# Get the list of all the .c files in the current directory
+files=$(ls *.c)
+
+# Change the .c files to object files
+objects=$(for file in $files; do echo "$file:o"; done)
+
+# Create the static library
+ar -rc liball.a $objects
+
+# Optimize the static library
+ranlib liball.a
