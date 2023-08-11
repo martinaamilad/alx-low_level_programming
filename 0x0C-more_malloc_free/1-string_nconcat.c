de4d294fe46d312a367391dee6a7be353ec411f2
martinaamilad <martina.m.mikhaeel@gmail.com>
2023-08-11 14:24:47 -0700

    Try

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,24 +21,22 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	int len1 = strlen(s1);
 	int len2 = n >= strlen(s2) ? strlen(s2) : n;
-
-  // Allocate memory for the new string.
-  char *new_str = malloc(len1 + len2 + 1);
-
-  // Initialize the new string to '\0'.
-  for (int i = 0; i < len1 + len2 + 1; i++) {
-    new_str[i] = '\0';
-  }
-
-  // Copy the first string into the new string.
-  for (int i = 0; i < len1; i++) {
-    new_str[i] = s1[i];
-  }
-
-  // Copy the first n bytes of the second string into the new string.
-  for (int i = 0; i < len2; i++) {
-    new_str[len1 + i] = s2[i];
-  }
-
+       
+	char *new_str = malloc(len1 + len2 + 1);
+	for (int i = 0; i < len1 + len2 + 1; i++)
+	{
+		new_str[i] = '\0';
+	}
+	
+	for (int i = 0; i < len1; i++)
+	{
+		new_str[i] = s1[i];
+	}
+	
+	for (int i = 0; i < len2; i++)
+	{
+		new_str[len1 + i] = s2[i];
+	}
+	
 	return (new_str);
 }
